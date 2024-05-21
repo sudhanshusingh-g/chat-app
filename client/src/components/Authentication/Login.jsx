756fca3828cbab1905925e82658f8dfa6916f717
sudhanshusingh-g <sudsingh32@gmail.com>
2024-05-21 18:51:20 +0530

    Login also implemented.

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,13 +1,78 @@
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { Button, FormControl, FormLabel } from "@chakra-ui/react";
-import { VStack, Input, InputRightElement, InputGroup } from "@chakra-ui/react";
+import {
+  VStack,
+  Input,
+  InputRightElement,
+  InputGroup,
+  useToast,
+  Button,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function Login() {
+  const toast = useToast();
+  const navigate = useNavigate();
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
+    const [loading, setLoading] = useState(false);
   //
-  const handleSubmit = () => {};
+  const handleSubmit = async() => {
+    setLoading(true);
+    if ( !email || !password) {
+      toast({
+        title: "Please fill all the required fields.",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        title: "Login successful",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+        position: "top-right",
+      });
+
+      localStorage.setItem("user", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Something bad happened!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      setLoading(false);
+    }
+
+
+  };
+
+
   return (
     <VStack spacing={4}>
       <FormControl id="email" isRequired>
@@ -41,6 +106,7 @@ function Login() {
         textColor="white"
         onClick={handleSubmit}
         width="100%"
+        isLoading={loading}
       >
         Login
       </Button>
