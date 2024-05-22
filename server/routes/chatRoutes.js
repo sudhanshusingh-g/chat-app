21ab7f4d5abc7f2f7ce9280021dd36fa0405ff26
sudhanshusingh-g <sudsingh32@gmail.com>
2024-05-22 23:14:24 +0530

    group chat api done

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const protect = require("../middlewares/authMiddleware");
-const {accessChat}=require("../controllers/chatController");
+const { accessChat, fetchChats,createGroupChat } = require("../controllers/chatController");
 const router=require("express").Router();
 
 router.route("/").post(protect,accessChat);
-// router.route("/").post(protect, fetchChat);
-// router.route("/group").post(protect,createGroupChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect,createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/removeGroupMember").post(protect, removeGroupMember);
 // router.route("/addGroupMember").post(protect, addGroupMember);
