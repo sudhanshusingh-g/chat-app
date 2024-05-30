be72f9e08fa9f2d7c54b5212d57bc7d1f67c2ba6
sudhanshusingh-g <sudsingh32@gmail.com>
2024-05-31 00:59:33 +0530

    Update npm start

--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ dbConnection();
 
 const PORT=process.env.PORT;
 
-app.get("/",(req,res)=>{
-    res.send("API running");
-})
+// app.get("/",(req,res)=>{
+//     res.send("API running");
+// })
 
 
 
@@ -34,7 +34,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
   );
 } else {
